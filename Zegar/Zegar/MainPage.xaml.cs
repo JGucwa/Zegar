c320179d48c2eeabaf97e770c1a6c3abc1a32c6b
JGucwa <kubjaxxx@gmail.com>
2024-03-04 10:42:50 +0100

    Dodano funkcje alarmu

--- Zegar/Zegar/MainPage.xaml.cs
+++ Zegar/Zegar/MainPage.xaml.cs
@@ -10,6 +10,7 @@ namespace Zegar
 {
     public partial class MainPage : TabbedPage
     {
+        private TimeSpan CurrentAlarm;
         public MainPage()
         {
             InitializeComponent();
@@ -19,12 +20,24 @@ namespace Zegar
         {
             Device.StartTimer(TimeSpan.FromSeconds(1), () =>
             {
+                string hour = DateTime.Now.ToString("HH");
+                string minute = DateTime.Now.ToString("mm");
+                string sencond = DateTime.Now.ToString("ss");
+                if (CurrentAlarm.ToString() == new TimeSpan(Convert.ToInt32(hour), Convert.ToInt32(minute), Convert.ToInt32(sencond)).ToString())
+                {
+                    DisplayAlert("Alarm", "Pobudka", "OK");
+                }
+
                 TimeDate.Text = DateTime.Now.ToString("HH:mm:ss");
                 Date.Text = DateTime.Now.ToString("dd MMMM yyyy");
                 SetCurrentTime();
                 return false;
             });
-
+        }
+        void SetAlarm(object sender, EventArgs e)
+        {
+            CurrentAlarm = Alarm.Time;
+            Alarm.Time = new TimeSpan();
         }
     }
 }
